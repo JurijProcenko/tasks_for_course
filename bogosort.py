2e5f6e5f8d5477d9add27e704d843a73351605d8
Jurij Procenko <elsoul@gmail.com>
2023-06-16 11:39:46 +0300

    Sort list of random 10 numbers by Bobo sort algorythm

---
+++ bogosort.py
@@ -0,0 +1,20 @@
+import random
+
+def is_sort(nums):
+    for i in range(len(nums) - 1):
+        if nums[i] > nums[i + 1]:
+            return False
+    return True
+
+def bogosort(nums):
+    while not is_sort(nums):
+        random.shuffle(nums)
+    return nums
+
+numbers = list(range(10))
+random.shuffle(numbers)
+print(numbers)
+
+sorted_numbers = bogosort(numbers)
+
+print(sorted_numbers)
